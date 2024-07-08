f8814bab1a2edab03c1f10482d6d88c39be04bf1
Erismar Oliveira <erismarpro@hotmail.com>
2024-07-08 12:55:21 -0300

    NSubstitute implementation

---
+++ Backend/ApiMicroservicesProductTest/UnitTests/ServicesNSubstitute/CategoryDtoServiceTest.cs
@@ -0,0 +1,100 @@
+﻿using ApiMicroservicesProduct.Dtos;
+using ApiMicroservicesProduct.Services.Interfaces;
+using NSubstitute;
+
+namespace ApiMicroservicesProductTest.UnitTests.ServicesNSubstitute;
+
+public class CategoryDtoServiceTest
+{
+    [Fact]
+    public async Task GetItemsDtoAsync_ReturnsCategoriesDto()
+    {
+        // Arrange
+        var categoryDtoService = Substitute.For<ICategoryDtoService>();
+
+        var categoriesDto = new List<CategoryDto>
+        {
+            new() { Id = 1, Name = "Category 1", Image = "image1.jpg" },
+            new() { Id = 2, Name = "Category 2", Image = "image2.jpg" }
+        };
+
+        categoryDtoService.GetItemsDtoAsync().Returns(categoriesDto);
+
+        // Act
+        var result = await categoryDtoService.GetItemsDtoAsync();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(categoriesDto.Count, result.Count());
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_ReturnsCategoryDto()
+    {
+        // Arrange
+        var categoryDtoService = Substitute.For<ICategoryDtoService>();
+
+        var categoryId = 1;
+        var categoryDto = new CategoryDto { Id = categoryId, Name = "Category 1", Image = "image1.jpg" };
+
+        categoryDtoService.GetByIdAsync(categoryId).Returns(categoryDto);
+
+        // Act
+        var result = await categoryDtoService.GetByIdAsync(categoryId);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(categoryId, result.Id);
+    }
+
+    [Fact]
+    public async Task AddAsync_AddNewCategory()
+    {
+        // Arrange
+        var categoryDtoService = Substitute.For<ICategoryDtoService>();
+
+        var categoryToAdd = new CategoryDto
+        {
+            Id = 1,
+            Name = "Category 1",
+            Image = "image1.jpg"
+        };
+
+        await categoryDtoService.AddAsync(categoryToAdd);
+
+        await categoryDtoService.Received().AddAsync(Arg.Any<CategoryDto>());
+    }
+
+    [Fact]
+    public async Task UpdateAsync_UpdateExistingCategory()
+    {
+        // Arrange
+        var categoryDtoService = Substitute.For<ICategoryDtoService>();
+
+        var categoryToUpdate = new CategoryDto
+        {
+            Id = 1,
+            Name = "Category 1",
+            Image = "image1.jpg"
+        };
+
+        await categoryDtoService.UpdateAsync(categoryToUpdate);
+
+        await categoryDtoService.Received().UpdateAsync(Arg.Any<CategoryDto>());
+    }
+
+    [Fact]
+    public async Task DeleteAsync_DeleteExistingCategory()
+    {
+        // Arrange
+        var categoryDtoService = Substitute.For<ICategoryDtoService>();
+
+        var categoryId = 1;
+
+        categoryDtoService.GetByIdAsync(categoryId).Returns(new CategoryDto { Id = categoryId });
+
+        await categoryDtoService.DeleteAsync(categoryId);
+
+        await categoryDtoService.Received(1).DeleteAsync(Arg.Any<int>());
+    }
+}
---
+++ Backend/ApiMicroservicesProductTest/UnitTests/ServicesNSubstitute/ProductDtoServiceTest.cs
@@ -0,0 +1,123 @@
+﻿using ApiMicroservicesProduct.Dtos;
+using ApiMicroservicesProduct.Services.Interfaces;
+using NSubstitute;
+
+namespace ApiMicroservicesProductTest.UnitTests.ServicesNSubstitute;
+
+public class ProductDtoServiceTest
+{
+    private static readonly string[] sourceArray1 = ["image1.jpg", "image2.jpg"];
+    private static readonly string[] sourceArray2 = ["image3.jpg", "image4.jpg"];
+
+    [Fact]
+    public async Task GetItemsDtoAsync_ReturnsProductsDto()
+    {
+        // Arrange
+        var productDtoService = Substitute.For<IProductDtoService>();
+
+        var productsDto = new List<ProductDto>
+        {
+            new()
+            {
+                Id = 1,
+                Name = "Product 1",
+                Images = [.. sourceArray1],
+                Description = "Description 1",
+                Price = 10.0M,
+                Stock = 10,
+                CategoryId = 1
+            },
+            new()
+            {
+                Id = 2,
+                Name = "Product 2",
+                Images = [.. sourceArray2],
+                Description = "Description 2",
+                Price = 20.0M,
+                Stock = 20,
+                CategoryId = 2
+            }
+        };
+
+        productDtoService.GetItemsDtoAsync().Returns(productsDto);
+
+        // Act
+        var result = await productDtoService.GetItemsDtoAsync();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(productsDto.Count, result.Count());
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_ReturnProductDto()
+    {
+        // Arrange
+        var productDtoService = Substitute.For<IProductDtoService>();
+        var productId = 1;
+
+        var productDto = new ProductDto
+        {
+            Id = 1,
+            Name = "Product 1",
+            Images = [.. sourceArray1],
+            Description = "Description 1",
+            Price = 10.0M,
+            Stock = 10,
+            CategoryId = 1
+        };
+
+        productDtoService.GetByIdAsync(productId).Returns(productDto);
+
+        // Act
+        var result = await productDtoService.GetByIdAsync(productId);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(productId, result.Id);
+    }
+
+    [Fact]
+    public async Task AddAsync_AddNewProduct()
+    {
+        // Arrange
+        var productDtoService = Substitute.For<IProductDtoService>();
+
+        var productToAdd = new ProductDto
+        {
+            Id = 1,
+            Name = "Product 1",
+            Images = [.. sourceArray1],
+            Description = "Description 1",
+            Price = 10.0M,
+            Stock = 10,
+            CategoryId = 1
+        };
+
+        await productDtoService.AddAsync(productToAdd);
+
+        await productDtoService.Received().AddAsync(Arg.Any<ProductDto>());
+    }
+
+    [Fact]
+    public async Task UpdateAsync_UpdateExistingProduct()
+    {
+        // Arrange
+        var productDtoService = Substitute.For<IProductDtoService>();
+
+        var productToUpdate = new ProductDto
+        {
+            Id = 1,
+            Name = "Product 1",
+            Images = [.. sourceArray1],
+            Description = "Description 1",
+            Price = 10.0M,
+            Stock = 10,
+            CategoryId = 1
+        };
+
+        await productDtoService.UpdateAsync(productToUpdate);
+
+        await productDtoService.Received().UpdateAsync(Arg.Any<ProductDto>());
+    }
+}
